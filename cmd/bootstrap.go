81220bf377cec64cc743c5426f3db226304b9c41
fengyunfei <fengyunfei@vchangyi.com>
2023-03-08 17:44:50 +0800

    结构优化

--- cmd/bootstrap.go
+++ cmd/bootstrap.go
@@ -28,7 +28,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "jd-cps",
+	Use:   "gobase",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 	examples and usage of using your application. For example:
@@ -44,21 +44,16 @@ var RootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	newCommandsBuilder().addCommands(
+		newJobcmd(),
+		newhttpcmd(),
+	).build()
 	cobra.CheckErr(RootCmd.Execute())
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "./config.yaml", "config file (default is ./config.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -80,7 +75,71 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
 	}
+	fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+
+}
+
+type cmder interface {
+	getCommand() *cobra.Command
+}
+
+func newCommandsBuilder() *commandsBuilder {
+	return &commandsBuilder{}
+}
+
+type commandsBuilder struct {
+	commands []cmder
+}
+
+func (b *commandsBuilder) addCommands(commands ...cmder) *commandsBuilder {
+	b.commands = append(b.commands, commands...)
+	return b
+}
+
+func addCommands(root *cobra.Command, commands ...cmder) {
+	for _, command := range commands {
+		subcmd := command.getCommand()
+		if subcmd == nil {
+			continue
+		}
+		root.AddCommand(subcmd)
+	}
+}
+
+func (b *commandsBuilder) build() *cobra.Command {
+	h := RootCmd
+	addCommands(h, b.commands...)
+	return h
+}
+
+type baseCmd struct {
+	cmd    *cobra.Command
+	config string
+}
+
+var _ commandsBuilderGetter = (*baseBuilderCmd)(nil)
+
+// Used in tests.
+type commandsBuilderGetter interface {
+	getCommandsBuilder() *commandsBuilder
+}
+
+type baseBuilderCmd struct {
+	*baseCmd
+	*commandsBuilder
+}
+
+func (b *baseBuilderCmd) getCommandsBuilder() *commandsBuilder {
+	return b.commandsBuilder
+}
+
+func (c *baseCmd) getCommand() *cobra.Command {
+	return c.cmd
+}
+
+func newBaseCmd(cmd *cobra.Command) *baseCmd {
+	return &baseCmd{cmd: cmd}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -12,74 +12,13 @@ import (
 var CfgData Config
 
 type Config struct {
-	Mode    string       `json:"mode" yaml:"mode"`
-	Mysql   xdb.DbConfig `json:"mysql"`
-	MiniApp MiniApp      `json:"mini_app"`
-	Kepler  KplConfig    `json:"kepler"`
-}
-
-type MiniApp struct {
-	Appid  string `json:"appid" yaml:"appid"`
-	Secret string `json:"secret" yaml:"secret"`
-}
-
-type KplConfig struct {
-	App     KplApp     `json:"app" yaml:"app"`
-	Account KplAccount `json:"account" yaml:"account"`
-	JCQ     KplJCQ     `json:"jcq" yaml:"jcq"`
-}
-
-type KplAccount struct {
-	Pin        string `json:"pin" yaml:"pin"`
-	ChannelId  int64  `json:"channel_id" yaml:"channel_id"`
-	CustomerId int64  `json:"customer_id" yaml:"customer_id"`
-	OpName     string `json:"op_name" yaml:"op_name"`
-}
-
-type KplApp struct {
-	Key    string `json:"key" yaml:"key"`
-	Secret string `json:"secret" yaml:"secret"`
-}
-
-type KplJCQ struct {
-	AccessKey    string `json:"access_key" yaml:"access_key"`
-	AccessSecret string `json:"access_secret" yaml:"access_secret"`
-	TenantId     int64  `json:"tenant_id" yaml:"tenant_id"`
-	GroupId      int64  `json:"group_id" yaml:"group_id"`
-}
-
-type PartnerMerchant struct {
-	ServiceMerchant
-	StandaloneMerchant
-}
-
-// ServiceMerchant 服务商户配置
-type ServiceMerchant struct {
-	SpAppId       string
-	SpMchId       string
-	SpSerialNum   string
-	SpPrivateKey  string
-	SpCertificate string
-	SpApiV3Key    string
-}
-
-// StandaloneMerchant 独立商户配置
-type StandaloneMerchant struct {
-	SubAppId   string
-	SubAppName string
-	SubMchId   string
-	SubKey     string
+	Mode  string       `json:"mode" yaml:"mode"`
+	Mysql xdb.DbConfig `json:"mysql"`
 }
 
 func InitConfigData() {
 	CfgData.Mode = viper.GetString("mode")
 	if err := utils.ViperGetNode("db", &CfgData.Mysql); err != nil {
-		panic(err)
-	}
-	if err := utils.ViperGetNode("mini_app", &CfgData.MiniApp); err != nil {
-		panic(err)
-	}
-	if err := utils.ViperGetNode("kepler", &CfgData.Kepler); err != nil {
 		panic(err)
 	}
 
